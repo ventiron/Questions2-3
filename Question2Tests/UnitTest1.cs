8b01add4f54d17d4c2abc5b391b703c39525ac6a
ventiron <leva_tishchenko@mail.ru>
2024-05-28 20:43:28 +0800

    Добавьте файлы проекта.

---
+++ Question2Tests/UnitTest1.cs
@@ -0,0 +1,62 @@
+using Questions2_3;
+
+namespace Question2Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TriangleArea()
+        {
+            double side1 = 5.3;
+            double side2 = 4.6;
+            double side3 = 5.1;
+
+            Figure triangle = new Triangle(side1, side2, side3);
+
+
+
+            double perimeter = side1 + side2 + side3;
+            double testArea = Math.Sqrt(perimeter * (perimeter - side1) * (perimeter - side2) * (perimeter - side3));
+
+
+
+            double area = triangle.Area();
+            Assert.AreEqual(Math.Round(area,8), Math.Round(testArea, 8)); //Точность - 8 знаков после запятой
+        }
+
+        [TestMethod]
+        public void RightTriangleArea()
+        {
+            double side1 = 3;
+            double side2 = 4;
+            double side3 = 5;
+
+            Figure rightTriangle = new Triangle(side1, side2, side3);
+
+
+
+            double testArea = side1 * side2 / 2;
+
+
+
+            double area = rightTriangle.Area();
+            Assert.AreEqual(Math.Round(area, 8), Math.Round(testArea, 8));
+        }
+
+        [TestMethod]
+        public void CircleArea()
+        {
+            double radius = 3.514;
+
+            Figure circle = new Circle(radius);
+
+
+
+            double testArea = Math.PI * radius * radius;
+
+            double area = circle.Area();
+            Assert.AreEqual(Math.Round(area, 8), Math.Round(testArea, 8));
+        }
+    }
+}
---
+++ Questions2-3/Question2.cs
@@ -0,0 +1,87 @@
+﻿namespace Questions2_3
+{
+
+    public interface Figure
+    {
+        public double Area();
+    }
+
+
+
+
+
+    public class Triangle : Figure
+    {
+        private List<double> Sides;
+
+        public Triangle(double Side1, double Side2, double Side3)
+        {
+            Sides = new List<double>(3);
+
+            Sides.Add(Side1);
+            Sides.Add(Side2);
+            Sides.Add(Side3);
+
+            Sides.Sort();
+
+            if (!IsPossible())
+            {
+                Console.WriteLine("Я бы тут выкинул ArgumentException, но пускай будет просто предупреждение:" +
+                    " треугольник с такими сторонами невозможен");
+            }
+        }
+
+
+        public double Area()
+        {
+            double area;
+
+
+            if (IsRight())
+            {
+                area = Sides[0] * Sides[1] / 2;
+                return area;
+            }
+
+            double perimeter = Sides.Sum();
+            area = Math.Sqrt(perimeter * (perimeter - Sides[0]) * (perimeter - Sides[1]) * (perimeter - Sides[2]));
+
+            return area;
+
+        }
+
+        private bool IsPossible()
+        {
+            bool st1 = Sides[0] + Sides[1] > Sides[2];
+            bool st2 = Sides[1] + Sides[2] > Sides[0];
+            bool st3 = Sides[2] + Sides[0] > Sides[1];
+
+            return st1 && st2 && st3;
+        }
+        private bool IsRight()
+        {
+            bool st = Math.Pow(Sides[0],2) + Math.Pow(Sides[1],2) == Math.Pow(Sides[2],2);
+            return st;
+        }
+    }
+
+
+
+
+
+    public class Circle : Figure
+    {
+        public double Radius;
+
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+
+        public double Area()
+        {
+            double area = Math.PI * Radius * Radius;
+            return area;
+        }
+    }
+}
